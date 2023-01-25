233e4417c3fc2f55d5d6ca5dc00d6abdd7008528
nterehin <nterehin@mobiledimension.ru>
2023-01-25 11:34:57 +0300

    1.0: increase boot time and add health check

--- src/main/kotlin/org/terexteam/memo/MemoApplication.kt
+++ src/main/kotlin/org/terexteam/memo/MemoApplication.kt
@@ -2,8 +2,11 @@ package org.terexteam.memo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.terexteam.memo.repository.MemoRepository
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = [MemoRepository::class])
 class MemoApplication
 
 fun main(args: Array<String>) {
